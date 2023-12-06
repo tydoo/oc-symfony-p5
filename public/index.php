434de12accb357aafa52531a5c43a89ca466e39f
BOYER Thomas <thomas.boyer2@interieur.gouv.fr>
2023-12-06 14:22:46 +0100

    ✨ Init project with composer #1

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use App\Kernel;
+
+$kernel = new Kernel();
+$kernel->run();
---
+++ src/Kernel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Kernel {
+    public function run() {
+    }
+}
