625ea8a8b666847d8adbf04a1e3c590582863dbb
BOYER Thomas <thomas.boyer2@interieur.gouv.fr>
2023-12-10 14:25:31 +0100

    ✅ Mise en place de variables d'environement dans un .env

--- public/index.php
+++ public/index.php
@@ -2,14 +2,7 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use Throwable;
 use App\Kernel;
-use App\Controller\ErrorController;
 
-try {
-    $kernel = new Kernel();
-    $kernel->run();
-} catch (Throwable $th) {
-    $error = new ErrorController();
-    $error->internalServerError();
-}
+$kernel = new Kernel();
+$kernel->run();
