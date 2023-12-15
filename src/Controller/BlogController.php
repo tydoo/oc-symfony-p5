37dd55786cb2ba0101b0207e10283eb6a831fdde
BOYER Thomas <thomas.boyer2@interieur.gouv.fr>
2023-12-15 21:58:29 +0100

    🍱 Ajout des templates de base

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Core\Route;
+use Core\Response\Response;
+
+class BlogController {
+
+    #[Route(path: '/blog', name: 'blog')]
+    public function blog(): Response {
+        return new Response('blog/liste.html.twig');
+    }
+
+    #[Route(path: '/blog/{id}', name: 'blog_show')]
+    public function show(int $id): Response {
+        return new Response('blog/blog_show.html.twig', ['id' => $id]);
+    }
+}
