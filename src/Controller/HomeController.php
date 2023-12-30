23226c8a2570848e40dc7dbea6a7d309ea36bec9
BOYER Thomas <thomas.boyer2@interieur.gouv.fr>
2023-12-30 11:46:03 +0100

    Remove dead code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use Core\Attribute\Route;
 use Core\Response\Response;
-use App\Repository\UserRepository;
-use App\Repository\LevelRepository;
 use Core\Response\RedirectResponse;
 
 class HomeController {
@@ -17,18 +15,6 @@ class HomeController {
 
     #[Route(path: '/home', name: 'home')]
     public function home(): Response {
-        $UserRepository = new UserRepository();
-        $LevelRepository = new LevelRepository();
-        $user = $UserRepository->find(2);
-        $user
-            ->setFirstname('John')
-            ->setLastname('Doe')
-            ->setEmail('johndoe@gmail.com')
-            ->setPassword('123456')
-            ->setLevel(
-                $LevelRepository->find(2)
-            );
-        $UserRepository->save($user);
         return new Response('home.html.twig');
     }
 }
